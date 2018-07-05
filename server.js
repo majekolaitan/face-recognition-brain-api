8e7014faa606344f9967363967eaec8b19405f80
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 13:39:32 +0400

    getting user profile - test

--- server.js
+++ server.js
@@ -76,11 +76,8 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
 	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
+	db.select('*').from('users').then(user => {
+		console.log(user);
 	})
 	if (!found) {
 		res.status(400).json('not found');
