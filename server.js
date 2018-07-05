f6e16d32696f56f210543f515916ea5532500529
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 12:32:40 +0400

    knex setup

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+});
 
 const app = express();
 
