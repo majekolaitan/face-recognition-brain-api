78100d7e464d6db1596f9908a1003a5772f02c60
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 15:53:02 +0400

    signin - validate user

--- server.js
+++ server.js
@@ -53,8 +53,17 @@ app.post('/signin', (req, res) => {
 	db.select('email', 'hash').from('login')
 	  .where('email', '=', req.body.email)
 	  .then(data => {
-	  	console.log(data);
+	  	const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+	  	if (isValid) {
+	  		return db.select('*').from('users')
+	  		.where('email', '=', req.body.email)
+	  		.then(user => {
+	  			res.json(user[0])
+	  		})
+	  		.catch(err => res.status(400).json('unable to get user'))
+	  	}
    })
+	  .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
