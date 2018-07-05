0046e53fc3b49667b0272004060237a19a34c11d
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 13:12:47 +0400

    test postman - return inserted user

--- server.js
+++ server.js
@@ -60,12 +60,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users').insert({
+	db('users')
+		.returning('*')
+		.insert({
 		email: email,
 		name: name,
 		joined: new Date()
-	}).then(console.log)
-	res.json(database.users[database.users.length - 1]);
+	})
+	.then(response => {
+		res.json(response);
+	})
 })
 
 app.get('/profile/:id', (req, res) => {
