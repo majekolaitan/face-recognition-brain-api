a4376701ccfee77beee92fa498dbf7a76a201c61
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 20:34:47 +0400

    moving clarifai to backend

--- controllers/image.js
+++ controllers/image.js
@@ -5,7 +5,7 @@ const app = new Clarifai.App({
 });
 
 app.models
-	.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
