c9b179af29a818920ab1cbeae0407f8b1ece01cb
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 18:43:09 +0400

    profile-id cleanup

--- server.js
+++ server.js
@@ -56,18 +56,7 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({id})
-	.then(user => {
-		if (user.length) {
-			res.json(user[0])
-		} else {
-			res.status(400).json('not found');
-		}
-	})
-	.catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', )
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
