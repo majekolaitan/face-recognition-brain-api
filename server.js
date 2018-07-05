9924ebef95d8c6910c7c374e884068ed16706070
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-05 18:53:42 +0400

    cleanup

--- server.js
+++ server.js
@@ -23,28 +23,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-
-	]
-}
-
 app.get('/', (req, res) => {
 	res.send(database.users);
 })
